5eb2c2bfa3dd986c1b1e37b5feafa52ffcf7dee7
Vitaliy Koval <vitaly.koval@gmail.com>
2023-01-13 23:38:09 +0200

    add security constraint

--- Component/Security/Constraint/SecurityConstraint.cs
+++ Component/Security/Constraint/SecurityConstraint.cs
@@ -1,9 +1,66 @@
 ﻿using System.Reflection;
-using Sencilla.Component.Users;
+using System.Linq.Dynamic.Core;
+using System.Text.RegularExpressions;
+
 using Sencilla.Core;
+using Sencilla.Component.Users;
+using System.Collections.Concurrent;
 
 namespace Sencilla.Component.Security
 {
+    public class ParsedConstraintCache
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private static ConcurrentDictionary<string, ParsedConstraint> Cache = new();
+
+        public static ParsedConstraint Get(string constraint)
+        {
+            // 
+            if (Cache.ContainsKey(constraint))
+                return Cache[constraint];
+
+            // 
+            var parsed = new ParsedConstraint(constraint);
+            Cache[constraint] = parsed;
+            return parsed;
+        }
+    }
+
+    public class ParsedConstraint
+    {
+        private List<string> Params = new();
+
+        public ParsedConstraint(string constraint) 
+        {
+            Constraint = constraint;
+            Parse(constraint);
+        }
+
+        public string Constraint { get; }
+
+        public object[] Vars(ISystemVariable sysVars) => Params.Select(p => sysVars.Get(p)).ToArray();
+
+        private void Parse(string constraint)
+        {
+            // replace all placeholders 
+            var idx = 0;
+            var replaced = Regex.Replace(constraint, "{[\\w\\s\\d.]*}", match =>
+            {
+                // add vars 
+                var val = match.Value.Trim(' ', '{', '}');
+                var var = new Regex("[\\s\\w\\d]*").Replace(val, m =>
+                {
+                    Params.Add(m.Value);
+                    return $"${idx++}";
+                }, 1);
+
+                return var;
+            });
+        }
+    }
+
     [Implement(typeof(IReadConstraint))]
     public class SecurityConstraint : Resolveable, IReadConstraint
     {
@@ -57,9 +114,13 @@ namespace Sencilla.Component.Security
                 // Apply constraints 
                 foreach (var a in accesses)
                 {
-                    var expr = ConstraintExpressionParser.Parse(a.Constraint)?.ToExpression<TEntity>();
-                    if (expr != null)
-                        query = query.Where(expr);
+                    if (!string.IsNullOrEmpty(a?.Constraint))
+                    {
+                        // 
+                        var sysVars = R<ISystemVariable>();
+                        var constr = ParsedConstraintCache.Get(a?.Constraint);
+                        query = query.Where(a.Constraint, constr.Vars(sysVars));
+                    }
                 }
             }
             return query;
