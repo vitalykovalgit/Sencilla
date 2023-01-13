e6a15626628906325169900ca3e21b445af0bf0b
Vitaliy Koval <vitaly.koval@gmail.com>
2023-01-13 23:35:29 +0200

    add system variables

---
+++ Core/SysVars/Contract/ISystemVariable.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Sencilla.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISystemVariable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        void Add(string name, object data);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        object Get(string name);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        T? Get<T>(string name);
+    }
+}
---
+++ Core/SysVars/Impl/SystemVariable.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Sencilla.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Implement(typeof(ISystemVariable), PerRequest = true)]
+    public class SystemVariable : ISystemVariable
+    {
+        Dictionary<string, object> Variables = new();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        public void Add(string name, object data)
+        {
+            Variables.Add(name, data);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public object? Get(string name)
+        { 
+            return Variables.ContainsKey(name) ? null : Variables[name];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public T? Get<T>(string name)
+        {
+            var obj = Variables.ContainsKey(name) ? null : Variables[name];
+            return (T?)obj;
+        }
+    }
+}
