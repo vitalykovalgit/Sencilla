e16299c18575f58364fec4e46629ce72eae96d51
Vitaliy Koval <vitaly.koval@gmail.com>
2024-04-11 11:35:11 +0300

    extend contact types

--- Component/Users/Entity/UserContactType.cs
+++ Component/Users/Entity/UserContactType.cs
@@ -2,8 +2,11 @@
 
 [CrudApi("api/v1/users/contacttypes")]
 [Table(nameof(UserContactType), Schema = "sec")]
-public class UserContactType: IEntity<byte>
+public class UserContactType: IEntity<byte>, IEntityOrderable<byte>, IEntityHideable 
 {
     public byte Id { get; set; }
     public required string Name { get; set; }
+    public string? Icon { get; set; }
+    public bool Hidden { get; set; }
+    public byte Order { get; set; }
 }
