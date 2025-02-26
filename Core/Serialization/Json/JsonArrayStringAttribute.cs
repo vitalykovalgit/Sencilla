7ca0cf3e1990aa0558c8c97217ce27e8e6a6fa34
Vitaliy Koval <vitaly.koval@gmail.com>
2025-02-26 17:04:53 +0200

    Add JsonArrayAttribute

---
+++ Core/Serialization/Json/JsonArrayStringAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Sencilla.Core;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class JsonArrayStringAttribute : JsonConverterAttribute
+{
+    public JsonArrayStringAttribute() : base(typeof(JsonArrayStringConverter)) { }
+}
---
+++ Core/Serialization/Json/JsonArrayStringConverter.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Sencilla.Core;
+
+/// <summary>
+/// Convert json to object and object to json
+/// </summary>
+public class JsonArrayStringConverter : JsonConverter<string>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType is JsonTokenType.StartArray)
+            return JsonSerializer.Serialize(JsonSerializer.Deserialize<object[]>(ref reader, options), options);
+
+        reader.Skip();
+        return default;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return;
+
+        var obj = JsonSerializer.Deserialize<object[]>(value, options);
+        JsonSerializer.Serialize(writer, obj, options);
+    }
+}
