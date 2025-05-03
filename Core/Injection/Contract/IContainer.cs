7899296f1308ce598230c33db443662654d317de
Vitaliy Koval <vitaly.koval@gmail.com>
2025-05-03 13:16:26 +0300

    Cosmetics changes

--- Core/Injection/Contract/IContainer.cs
+++ Core/Injection/Contract/IContainer.cs
@@ -1,30 +1,29 @@
 ﻿
-namespace Sencilla.Core
+namespace Sencilla.Core;
+
+/// <summary>
+/// Register type in container 
+/// </summary>
+public interface IContainer
 {
-    /// <summary>
-    /// Register type in container 
-    /// </summary>
-    public interface IContainer
-    {
-        IContainer RegisterInstance(Type service, object instance);
-        IContainer RegisterInstance<TService>(TService instance) where TService: class;
+    IContainer RegisterInstance(Type service, object instance);
+    IContainer RegisterInstance<TService>(TService instance) where TService: class;
 
-        IContainer RegisterType(Type service, string? name = null);
-        IContainer RegisterTypeSingleton(Type service, string? name = null);
-        IContainer RegisterTypePerRequest(Type service, string? name = null);
+    IContainer RegisterType(Type service, string? name = null);
+    IContainer RegisterTypeSingleton(Type service, string? name = null);
+    IContainer RegisterTypePerRequest(Type service, string? name = null);
 
-        IContainer RegisterType(Type service, Type implementation, string? name = null);
-        IContainer RegisterTypeSingleton(Type service, Type implementation, string? name = null);
-        IContainer RegisterTypePerRequest(Type service, Type implementation, string? name = null);
+    IContainer RegisterType(Type service, Type implementation, string? name = null);
+    IContainer RegisterTypeSingleton(Type service, Type implementation, string? name = null);
+    IContainer RegisterTypePerRequest(Type service, Type implementation, string? name = null);
 
-        IContainer RegisterType<TService>(string? name = null) where TService: class;
-        IContainer RegisterType<TService>(Func<IResolver, TService> implementationFactory) where TService : class;
-        IContainer RegisterTypeSingleton<TService>(string? name = null) where TService: class;
-        IContainer RegisterTypePerRequest<TService>(string? name = null) where TService: class;
+    IContainer RegisterType<TService>(string? name = null) where TService: class;
+    IContainer RegisterType<TService>(Func<IResolver, TService> implementationFactory) where TService : class;
+    IContainer RegisterTypeSingleton<TService>(string? name = null) where TService: class;
+    IContainer RegisterTypePerRequest<TService>(string? name = null) where TService: class;
 
 
-        IContainer RegisterType<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
-        IContainer RegisterTypeSingleton<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
-        IContainer RegisterTypePerRequest<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
-    }
+    IContainer RegisterType<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
+    IContainer RegisterTypeSingleton<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
+    IContainer RegisterTypePerRequest<TService, TImplementation>(string? name = null) where TService : class where TImplementation : class, TService;
 }
--- Core/Injection/Contract/IResolver.cs
+++ Core/Injection/Contract/IResolver.cs
@@ -1,40 +1,39 @@
 ﻿
-namespace Sencilla.Core
+namespace Sencilla.Core;
+
+
+/// <summary>
+/// Resolve the instance of the type 
+/// </summary>
+public interface IResolver
 {
+    /// <summary>
+    /// Resolve instance of specific type 
+    /// </summary>
+    /// <typeparam name="TType"> Type to be resolved </typeparam>
+    /// <returns> Instance of TType </returns>
+    TServcie? Resolve<TServcie>();
 
     /// <summary>
-    /// Resolve the instance of the type 
+    /// Resolve type with specofoc instance
     /// </summary>
-    public interface IResolver
-    {
-        /// <summary>
-        /// Resolve instance of specific type 
-        /// </summary>
-        /// <typeparam name="TType"> Type to be resolved </typeparam>
-        /// <returns> Instance of TType </returns>
-        TServcie? Resolve<TServcie>();
+    /// <typeparam name="TType">Type to be resolved </typeparam>
+    /// <param name="name"> Name of the instance </param>
+    /// <returns>Instance of the type</returns>
+    TServcie? Resolve<TServcie>(string name);
 
-        /// <summary>
-        /// Resolve type with specofoc instance
-        /// </summary>
-        /// <typeparam name="TType">Type to be resolved </typeparam>
-        /// <param name="name"> Name of the instance </param>
-        /// <returns>Instance of the type</returns>
-        TServcie? Resolve<TServcie>(string name);
+    /// <summary>
+    /// Resolve instance of specific type 
+    /// </summary>
+    /// <param name="type"> Type to be resolved </param>
+    /// <returns> Instance of specific type </returns>
+    object? Resolve(Type type);
 
-        /// <summary>
-        /// Resolve instance of specific type 
-        /// </summary>
-        /// <param name="type"> Type to be resolved </param>
-        /// <returns> Instance of specific type </returns>
-        object? Resolve(Type type);
+    /// <summary>
+    /// Resolve all instance of provided TType 
+    /// </summary>
+    /// <typeparam name="TType">Type to resolve</typeparam>
+    /// <returns> Collection of instances </returns>
+    IEnumerable<TServcie> ResolveAll<TServcie>();
 
-        /// <summary>
-        /// Resolve all instance of provided TType 
-        /// </summary>
-        /// <typeparam name="TType">Type to resolve</typeparam>
-        /// <returns> Collection of instances </returns>
-        IEnumerable<TServcie> ResolveAll<TServcie>();
-
-    }
 }
