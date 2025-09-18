459bbe72051efeb886c939223e50b21eec7a61e2
Vitaliy Koval <vitaly.koval@gmail.com>
2025-09-19 01:16:15 +0300

    Remove TickerQ

--- libs/messaging/Scheduler/Boostrap.cs
+++ libs/messaging/Scheduler/Boostrap.cs
@@ -1,9 +1,9 @@
 ﻿global using Sencilla.Messaging;
 global using Sencilla.Messaging.Scheduler;
 
-global using TickerQ.DependencyInjection;
-global using TickerQ.DependencyInjection.Hosting;
-global using TickerQ.Utilities.Interfaces;
+//global using TickerQ.DependencyInjection;
+//global using TickerQ.DependencyInjection.Hosting;
+//global using TickerQ.Utilities.Interfaces;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -12,14 +12,14 @@ public static class Bootstrap
     public static MessagingConfig UseScheduler(this MessagingConfig builder, Action<SchedulerProviderConfig>? config = null)
     {
         builder.AddProviderConfigOnce<SchedulerProviderConfig>(config);
-        if (builder.IsTypeNotRegistered<ITickerHost>())
-            builder.Services.AddTickerQ();
+        //if (builder.IsTypeNotRegistered<ITickerHost>())
+        //    builder.Services.AddTickerQ();
 
-        builder.AddAppBuilderOnce((app, host) =>
-        {
-            app?.UseTickerQ();
-            host?.UseTickerQ();
-        });
+        //builder.AddAppBuilderOnce((app, host) =>
+        //{
+        //    app?.UseTickerQ();
+        //    host?.UseTickerQ();
+        //});
 
         return builder;
     }
