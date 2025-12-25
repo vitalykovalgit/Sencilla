25e70b79d113badd9536a33e99c569663a6228ab
Valentina <tina.tinko01@gmail.com>
2025-12-25 14:15:10 +0400

    Resolved comments

---
+++ libs/core/Serialization/Json/JsonArrayEnumAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Sencilla.Core.Serialization.Json
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class JsonArrayEnumAttribute : JsonConverterAttribute
+    {
+        public JsonArrayEnumAttribute(Type enumType): base(typeof(JsonArrayEnumConverter<>).MakeGenericType(enumType))
+        {
+            if (!enumType.IsEnum)
+                throw new ArgumentException(nameof(enumType));
+        }
+    }
+}
