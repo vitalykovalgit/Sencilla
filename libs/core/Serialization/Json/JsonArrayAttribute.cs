80d0fd91c1a37fa4f6677f5759c4bae10ed0ed38
Vitaliy Koval <vitaly.koval@gmail.com>
2025-12-26 11:32:46 +0200

    Rename JsonArrayEnum to JsonArray

---
+++ libs/core/Serialization/Json/JsonArrayAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Sencilla.Core.Serialization.Json;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class JsonArrayAttribute : JsonConverterAttribute
+{
+    public JsonArrayAttribute(Type enumType): base(typeof(JsonArrayConverter<>).MakeGenericType(enumType))
+    {
+        if (!enumType.IsEnum)
+            throw new ArgumentException(nameof(enumType));
+    }
+}
