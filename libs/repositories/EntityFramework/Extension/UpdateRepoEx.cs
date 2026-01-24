5efe5872d1bbbb9be585d95b40f90d1102ba60c1
Vitaliy Koval <vitaly.koval@gmail.com>
2026-01-24 16:14:29 +0200

    Extend update ext for one item

--- libs/repositories/EntityFramework/Extension/UpdateRepoEx.cs
+++ libs/repositories/EntityFramework/Extension/UpdateRepoEx.cs
@@ -80,5 +80,95 @@ public static class UpdateRepoEx
         var query = repository.Query.Where(e => ids.Contains(e.Id)).ExecuteUpdateAsync(s => setPropertyCalls(s));
         return query;
     }
+
+    /// <summary>
+    /// Executes an update query on a single entity by its ID (default int key).
+    /// Internally translates to: Query.Where(e => e.Id == id).ExecuteUpdateAsync(setPropertyCalls)
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <param name="repository">The update repository</param>
+    /// <param name="id">The ID of the entity to update</param>
+    /// <param name="setPropertyCalls">Lambda expression defining property updates using EF Core's SetProperty method</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of entities affected by the update (0 or 1)</returns>
+    public static Task<int> ExecuteUpdateAsync<TEntity>(
+        this IUpdateRepository<TEntity> repository,
+        int id,
+        Action<UpdateSettersBuilder<TEntity>> setPropertyCalls,
+        CancellationToken token = default) where TEntity : class, IEntity<int>
+    {
+        return ExecuteUpdateAsync<TEntity, int>(repository, id, setPropertyCalls, token);
+    }
+
+    /// <summary>
+    /// Executes an update query on a single entity by its ID.
+    /// Internally translates to: Query.Where(e => e.Id == id).ExecuteUpdateAsync(setPropertyCalls)
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TKey">The key type</typeparam>
+    /// <param name="repository">The update repository</param>
+    /// <param name="id">The ID of the entity to update</param>
+    /// <param name="setPropertyCalls">Lambda expression defining property updates using EF Core's SetProperty method</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of entities affected by the update (0 or 1)</returns>
+    /// <remarks>
+    /// This method generates an efficient SQL UPDATE statement with a WHERE clause.
+    /// The setPropertyCalls parameter should be a lambda expression like:
+    /// s => s.SetProperty(e => e.Property1, value1).SetProperty(e => e.Property2, value2)
+    /// </remarks>
+    public static Task<int> ExecuteUpdateAsync<TEntity, TKey>(
+        this IUpdateRepository<TEntity, TKey> repository,
+        TKey id,
+        Action<UpdateSettersBuilder<TEntity>> setPropertyCalls,
+        CancellationToken token = default) where TEntity : class, IEntity<TKey>
+    {
+        var query = repository.Query.Where(e => e.Id!.Equals(id)).ExecuteUpdateAsync(s => setPropertyCalls(s));
+        return query;
+    }
+
+    /// <summary>
+    /// Executes an update query on a single entity (extracts ID from entity).
+    /// Internally translates to: Query.Where(e => e.Id == entity.Id).ExecuteUpdateAsync(setPropertyCalls)
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <param name="repository">The update repository</param>
+    /// <param name="entity">The entity to update (only ID is extracted and used)</param>
+    /// <param name="setPropertyCalls">Lambda expression defining property updates using EF Core's SetProperty method</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of entities affected by the update (0 or 1)</returns>
+    public static Task<int> ExecuteUpdateAsync<TEntity>(
+        this IUpdateRepository<TEntity> repository,
+        TEntity entity,
+        Action<UpdateSettersBuilder<TEntity>> setPropertyCalls,
+        CancellationToken token = default) where TEntity : class, IEntity<int>
+    {
+        return ExecuteUpdateAsync<TEntity, int>(repository, entity, setPropertyCalls, token);
+    }
+
+    /// <summary>
+    /// Executes an update query on a single entity (extracts ID from entity).
+    /// Internally translates to: Query.Where(e => e.Id == entity.Id).ExecuteUpdateAsync(setPropertyCalls)
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TKey">The key type</typeparam>
+    /// <param name="repository">The update repository</param>
+    /// <param name="entity">The entity to update (only ID is extracted and used)</param>
+    /// <param name="setPropertyCalls">Lambda expression defining property updates using EF Core's SetProperty method</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of entities affected by the update (0 or 1)</returns>
+    /// <remarks>
+    /// This method generates an efficient SQL UPDATE statement with a WHERE clause.
+    /// The setPropertyCalls parameter should be a lambda expression like:
+    /// s => s.SetProperty(e => e.Property1, value1).SetProperty(e => e.Property2, value2)
+    /// </remarks>
+    public static Task<int> ExecuteUpdateAsync<TEntity, TKey>(
+        this IUpdateRepository<TEntity, TKey> repository,
+        TEntity entity,
+        Action<UpdateSettersBuilder<TEntity>> setPropertyCalls,
+        CancellationToken token = default) where TEntity : class, IEntity<TKey>
+    {
+        var query = repository.Query.Where(e => e.Id!.Equals(entity.Id)).ExecuteUpdateAsync(s => setPropertyCalls(s));
+        return query;
+    }
 }
 
