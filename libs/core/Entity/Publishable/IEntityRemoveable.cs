522b024ee07002d896ad89e7b68691027d7cb252
Vitaliy Koval <vitaly.koval@gmail.com>
2026-01-27 12:57:56 +0200

    Add IEntityPublishable

---
+++ libs/core/Entity/Publishable/IEntityRemoveable.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Sencilla.Core;
+
+/// <summary>
+/// Item can be marked in DB as deleted 
+/// </summary>
+public interface IEntityPublishable: IEntityUpdateable
+{
+    public byte PublishStatus { get; set; }
+    public string? PublishService { get; set; }
+    public DateTime? PublishStartDate { get; set; }
+    public DateTime? PublishFinishDate { get; set; }
+
+    public byte[] RawVersion { get; set; }
+}
